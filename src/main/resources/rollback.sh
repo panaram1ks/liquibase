ea1dd1498f3f7ae7f799153ef2c229898811b6a2
parom <E.Parominskiy@isida.by>
2023-08-15 09:24:51 +0300

    rollback script

---
+++ src/main/resources/rollback.sh
@@ -0,0 +1,1 @@
+liquibase --driver=com.mysql.cj.jdbc.Driver --classpath="/home/parom/IdeaProjects/LiquibaseLearning/files/mysql-connector-j-8.1.0.jar" --changeLogFile=db-changelog.xml --url="jdbc:mysql://localhost:3306/DATING" --username=webuser --password=panaram1ks rollbackCount 1
